c91ef8c14c636cf34cf97719da1a298d9b4d0bdf
AkshatNaruka <narukaakshat@gmail.com>
2023-03-07 13:24:00 +0530

    major backend starts

---
+++ Snoke/HelpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HelpViewController.swift
+//  Snoke
+//
+//  Created by Akshat Naruka on 07/03/23.
+//
+
+import UIKit
+import AVKit
+class HelpViewController: UIViewController {
+
+    @IBOutlet var breathwork: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    fileprivate func addPlayerToView(_ view: UIView) {
+        let videoURL=NSURL(string: "https://www.youtube.com/watch?v=inpok4MKVLM")!
+        
+       }
+}
