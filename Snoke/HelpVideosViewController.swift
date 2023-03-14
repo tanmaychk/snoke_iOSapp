8b47efabce4f456d8e79ac34ff5c240a7914fdc0
AkshatNaruka <narukaakshat@gmail.com>
2023-03-14 23:29:36 +0530

    segmentation added in rewards and help

---
+++ Snoke/HelpVideosViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HelpVideosViewController.swift
+//  Snoke
+//
+//  Created by Akshat Naruka on 14/03/23.
+//
+
+import UIKit
+import WebKit
+class HelpVideosViewController: UIViewController, WKNavigationDelegate {
+
+    
+    @IBOutlet var webview1: WKWebView!
+    
+    @IBOutlet var webview2: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+}
--- Snoke/HelpViewController.swift
+++ Snoke/HelpViewController.swift
@@ -6,76 +6,33 @@
 //
 
 import UIKit
-import WebKit
-class HelpViewController: UIViewController,WKNavigationDelegate {
+class HelpViewController: UIViewController {
     
-    @IBOutlet var webView: WKWebView!
+    @IBOutlet var view1: UIView!
     
-    @IBOutlet var Physical: WKWebView!
+    @IBOutlet var view2: UIView!
+    
+    @IBOutlet var segmentationButton: UISegmentedControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //code for breathing webview
-        webView.frame = view.bounds
-        webView.navigationDelegate = self
-        let url = URL(string: "https://www.youtube.com/watch?v=inpok4MKVLM")!
-        let urlRequest = URLRequest(url: url)
-        
-        webView.load(urlRequest)
-        webView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
-        view.addSubview(webView)
-        
-        //code for physical webview
-        Physical.frame = view.bounds
-        Physical.navigationDelegate = self
-        let url1 = URL(string: "https://www.youtube.com/watch?v=M0uO8X3_tEA")!
-        let urlRequest1 = URLRequest(url: url1)
-        
-        Physical.load(urlRequest1)
-        Physical.autoresizingMask = [.flexibleWidth,.flexibleHeight]
-        view.addSubview(webView)
-        
-        
-        
         
     }
+
     
-    
-    
-    
-//    Physical.frame = view.bounds
-//    Physical.navigationDelegate = self
-//    let url = URL(string: "https://www.youtube.com/watch?v=inpok4MKVLM")!
-//    let urlRequest = URLRequest(url: url)
-//
-//    Physical.load(urlRequest)
-//    Physical.autoresizingMask = [.flexibleWidth,.flexibleHeight]
-//            view.addSubview(webView)
-//}
-    
-//    fileprivate func addPlayerToView(_ view: UIView) {
-//        let videoURL=NSURL(string: "https://www.youtube.com/watch?v=inpok4MKVLM")!
-//
-//       }
-//    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-//            if navigationAction.navigationType == .linkActivated  {
-//                if let url = navigationAction.request.url,
-//                    let host = url.host, !host.hasPrefix("www.google.com"),
-//                    UIApplication.shared.canOpenURL(url) {
-//                    UIApplication.shared.open(url)
-//                    print(url)
-//                    print("Redirected to browser. No need to open it locally")
-//                    decisionHandler(.cancel)
-//                    return
-//                } else {
-//                    print("Open it locally")
-//                    decisionHandler(.allow)
-//                    return
-//                }
-//            } else {
-//                print("not a user click")
-//                decisionHandler(.allow)
-//                return
-//            }
-//        }
+    @IBAction func segmentControlCLick(_ sender: UISegmentedControl){
+        switch segmentationButton.selectedSegmentIndex{
+        case 0:
+            view1.isHidden = false;
+            view2.isHidden = true;
+            segmentationButton.selectedSegmentTintColor = .red
+        case 1:
+            view2.isHidden = false;
+            view1.isHidden = true;
+            segmentationButton.selectedSegmentTintColor = .yellow
+        default:
+            break
+        }
+    }
 }
